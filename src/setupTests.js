6b16433bc44e0ef1818c9a0a872c23d2a10df70f
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2019-02-13 17:42:04 -0300

    Added react tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,11 @@
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({
+  adapter: new Adapter()
+})
+
+
 /*
   Fixes axios not be able to send headers
   See: https://stackoverflow.com/questions/42677387/jest-returns-network-error-when-doing-an-authenticated-request-with-axios#answer-43020260
