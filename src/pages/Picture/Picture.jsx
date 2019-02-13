0c462e5dd4f0745a068745a8ddd1a44bd99df677
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2019-02-13 19:47:40 -0300

    Added mock pages

---
+++ src/pages/Picture/Picture.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Picture = ({}) => {
+  return (
+    <div>
+      <h1>content</h1>
+    </div>
+  )
+}
+
+export default Picture
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Feed from 'pages/Feed/Feed'
+import Picture from 'pages/Picture/Picture'
+import Signup from 'pages/Signup/Signup'
+
+export {
+  Feed,
+  Picture,
+  Signup
+}
