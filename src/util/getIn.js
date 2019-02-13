1d2489d2a57f7ad7a7a8fb0307b716c595799295
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2019-02-13 20:06:46 -0300

    Fixed `in` operator bug

--- src/util/getIn.js
+++ src/util/getIn.js
@@ -1,8 +1,9 @@
+const canTraverse = (target, key) => target != null && key in target
+
 export default function getIn (object, path = '', defaultValue = undefined) {
   const result = object == null
     ? undefined
-    : path.split('.').reduce((current, key) =>
-        key in current ? current[key] : undefined,
+    : path.split('.').reduce((current, key) => canTraverse(current, key) ? current[key] : undefined,
       object
     )
 
