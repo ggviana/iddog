2a23317dd009c4f2995f1ecbc1e9fb0eb70e769a
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2019-02-13 23:15:47 -0300

    Adding token for fututer requests

--- src/contexts/user.js
+++ src/contexts/user.js
@@ -1,15 +1,28 @@
 import { create } from 'reworm'
 import LocalStorage from 'services/LocalStorage'
+import { setToken } from 'services/DogAPI'
 
 const AUTO_LOGOUT_TIMEOUT = 4000
 
+const applyToken = user => {
+  if (user && user.token) {
+    setToken(user.token)
+  } else {
+    setToken(null)
+  }
+}
+
+const user = LocalStorage.get('_u')
+applyToken(user)
+
 export const { get, select, set } = create({
-  user: LocalStorage.get('_u') || null
+  user: user || null
 })
 
 export const getCurrentUser = select(({ user }) => user)
 export const setCurrentUser = user => {
   LocalStorage.set('_u', user)
+  applyToken(user)
   set({ user })
 }
 
