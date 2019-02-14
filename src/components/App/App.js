9e2838bffa041579befb4444a7ff69e1d0d9ca23
Guilherme Guimarães Viana <ggviana@hotmail.com.br>
2019-02-14 18:13:14 -0300

    Fixed state handling

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Routes } from 'components'
+import { Provider as RewormProvider } from 'reworm'
 
 const App = () => (
-  <main>
-    <Routes />
-  </main>
+  <RewormProvider>
+    <main>
+      <Routes />
+    </main>
+  </RewormProvider>
 )
 
 export default App
